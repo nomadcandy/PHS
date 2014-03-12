9f888e0cac3e36e0765471c826f39c415c8e93d1
Jami Sue Becker <jamisuebecker@Jamis-MacBook-Pro.local>
2014-03-12 11:13:50 -0400

    Layout Interactive Page

--- Millenium/InteractiveViewController.m
+++ Millenium/InteractiveViewController.m
@@ -74,7 +74,7 @@
 
 - (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSUInteger)index reusingView:(UIView *)view
 {
-    
+    if (carousel==carouselSize){
     
 	UIButton *matSizeButton = (UIButton *)view;
 	if (matSizeButton == nil)
@@ -193,7 +193,132 @@
 
     return matSizeButton;
 }
+    
 
+
+else {
+    
+    UIButton *matColorButton = (UIButton *)view;
+	if (matColorButton == nil)
+	{
+		//no button available to recycle, so create new one
+		UIImage *image = [UIImage imageNamed:@"greySizeImage.png"];
+		matColorButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        matColorButton.frame = CGRectMake(0.0f, 0.0f, image.size.width/1.5, image.size.height/1.5);
+		[matColorButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+		[matColorButton setBackgroundImage:image forState:UIControlStateNormal];
+        
+        matColorButton.layer.shadowColor = [UIColor blackColor].CGColor;
+        matColorButton.layer.shadowOpacity = 0.8;
+        matColorButton.layer.shadowRadius = 12;
+        matColorButton.layer.shadowOffset = CGSizeMake(12.0f, 12.0f);
+        
+        matColorButton.titleLabel.shadowOffset = CGSizeMake(1.0, 1.0);
+        [matColorButton setContentVerticalAlignment:UIControlContentVerticalAlignmentCenter];
+        
+        [matColorButton.titleLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Bold" size:17.0]];
+		//button.titleLabel.font = [button.titleLabel.font fontWithSize:22];
+        [matColorButton addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+	}
+	
+	//set button label
+	[matColorButton setTitle:[NSString stringWithFormat:@"%i", index] forState:UIControlStateNormal];
+    [matColorButton setTitle:[NSString stringWithFormat:@"matSizeString", matSizeString] forState:UIControlStateNormal];
+    
+    
+    if (index ==0){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        //[button addTarget:self action:@selector(buttonTapped0:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    if (index ==1){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index ==2){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 3){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 4){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 5){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 6){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+        
+    }
+    
+    if (index ==7){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        //[button addTarget:self action:@selector(buttonTapped0:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    if (index ==8){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index ==9){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 10){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 11){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 12){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+    }
+    
+    if (index == 13){
+        
+        [matColorButton setTitle:[NSString stringWithFormat:@"red 001"] forState:UIControlStateNormal];
+        
+        
+    }
+    
+    
+    return matColorButton;
+    
+}
+
+    
+}
 
 
 
