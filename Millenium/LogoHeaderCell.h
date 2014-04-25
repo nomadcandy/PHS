58b9a70facebdcbc812b8c1a3737e25e2edad85d
Jami Sue Becker <jamisuebecker@Jamis-MacBook-Pro.local>
2014-04-25 15:07:57 -0400

    fixed PNG added cell Tags

--- Millenium/LogoHeaderCell.h
+++ Millenium/LogoHeaderCell.h
@@ -11,6 +11,8 @@
 @interface LogoHeaderCell : UICollectionViewCell
 
 @property (strong, nonatomic) IBOutlet UILabel *pageTitleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *logoTitleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *matTitleLabel;
 @property (strong, nonatomic) IBOutlet UIButton *backButton;
 @property (strong, nonatomic) IBOutlet UIButton *cintasButton;
 @property (strong, nonatomic) IBOutlet UIImageView *blackBarView;
