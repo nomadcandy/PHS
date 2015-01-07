04636179e24ac6575ee57eaa1bbcf160b54a4aab
nomadcandy <oclipse@yahoo.com>
2015-01-07 11:57:47 -0500

    mail button works

--- Millenium/DraftNotesCell.h
+++ Millenium/DraftNotesCell.h
@@ -8,6 +8,24 @@
 
 #import <CoreData/CoreData.h>
 
-@interface DraftNotesCell : NSManagedObject
+@interface DraftNotesCell : UICollectionViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *draftNoteTitleLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *draftNoteImage;
+//@property (strong, nonatomic) IBOutlet UIButton *draftChooseButton;
+@property (strong, nonatomic) IBOutlet UIButton *draftNoteChooseButton;
+
+@property (strong, nonatomic) IBOutlet UIButton *goBack;
+@property (strong, nonatomic) IBOutlet UIButton *goBackLogo;
+
+@property (nonatomic,strong) IBOutlet UILabel *goBackLogoLabel;
+@property (nonatomic,strong) IBOutlet UILabel *goBackMatLabel;
+
+
+
+@property (nonatomic,strong) IBOutlet UIButton *removeDraftButton;
+@property (nonatomic,strong) IBOutlet UILabel *removeDraftLabel;
+@property (nonatomic,strong) IBOutlet UILabel *addDraftLabel;
+
 
 @end
--- Millenium/DraftNotesCell.m
+++ Millenium/DraftNotesCell.m
@@ -10,4 +10,15 @@
 
 @implementation DraftNotesCell
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
 @end
